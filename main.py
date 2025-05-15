701d0cbd5ea37ed5ae6c52f5e09efd8b9a4c00a3
sebas295210 <bsebasfiallosm@gmail.com>
2025-05-14 20:34:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-import random  # Importamos la librería random para elegir elementos al azar
-# Lista de caracteres que se pueden usar en la contraseña
-caracteres = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-# Pedimos al usuario que elija la longitud de la contraseña
-longitud_contraseña = int(input("Introduzca la longitud del pase: "))
-contraseña = ""  # Creamos una variable vacía donde guardaremos la contraseña
-# Repetimos el proceso 'pass_length' veces para crear la contraseña
-for i in range(longitud_contraseña):  
-    contraseña += random.choice(caracteres)  # Elegimos un carácter aleatorio y lo agregamos a la contraseña
-print(contraseña)  # Mostramos la contraseña generada
+import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def en_lines():
+    print(f'Tu bot {bot.user} esta en linea')
+    
+@bot.command()
+async def saludar(ctx,*,mensaje:str):
+    
+    mensaje = mensaje.lower().strip()
+    
+    if mensaje == 'hola':
+        await ctx.send('Hola, ¿cómo estás?')
+        
+    elif mensaje == 'klk':
+        
+        await ctx.send('Klk, ¿todo bien?')
+        
+    elif mensaje == 'buenas':
+            
+        await ctx.send('Buenas, ¿qué tal?')
+    
+    else:
+        
+        await ctx.send('SALUDA BIEN!!!')
+@bot.command()
+async def sumar(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+    
+@bot.command()
+async def restar(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left - right)
+        
+token = ''
+        
+bot.run(token)
